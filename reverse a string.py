99d81af16defb202da8d21de2cf60432e22fc4a0
Sharath Kalappa <122499332+iamnotsharath@users.noreply.github.com>
2025-02-17 13:47:19 -0500

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,8 @@
+def reverse_string(my_string: str) ->str:
+    # Your code goes here
+    return my_string [::-1]
+print (reverse_string("this is not what I expected"))
+my_string = "hello hello 1"
+print (my_string)
+
+print (check_strings("ehlloo", my_string))
