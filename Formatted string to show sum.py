56d5ac31d95d9bfa862a8751e8a141696d17323d
Sharath Kalappa <122499332+iamnotsharath@users.noreply.github.com>
2025-02-17 14:52:16 -0500

    Add files via upload

---
+++ Formatted string to show sum.py
@@ -0,0 +1,10 @@
+# Python code​​​​​​‌‌​​‌​‌‌​‌​‌​‌‌​‌​​‌‌‌​‌​ below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = True
+show_hints = False
+
+def calc_string(first_number: int, second_number: int ) -> str:
+    # Your code goes here
+    sum = first_number+second_number
+    return f"The sum of {first_number} and {second_number} is {sum}."
